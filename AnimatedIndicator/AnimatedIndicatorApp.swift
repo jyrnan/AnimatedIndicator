3396f9992d8fe5e6c0e2b2b89f9dba85b01fcecb
jyrnan <yoeking@gmail.com>
2021-09-11 10:52:24 +0800

    Initial Commit

---
+++ AnimatedIndicator/AnimatedIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedIndicatorApp.swift
+//  AnimatedIndicator
+//
+//  Created by jyrnan on 2021/9/11.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
